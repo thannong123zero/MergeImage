f8eacf8d8c150d21fda156f85ee5b4a2ec7fe262
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-05-16 09:43:38 +0700

    update code

--- MergeImage/Controllers/HomeController.cs
+++ MergeImage/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using IronBarCode;
 using MergeImage.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using OfficeOpenXml;
 using System.Diagnostics;
 using System.Drawing;
 
@@ -19,37 +21,15 @@ namespace MergeImage.Controllers
 
         public IActionResult Index()
         {
-            // Load the large image
-            Image largeImage = Image.FromFile("wwwroot/images/6aee5048cca06dfe34b1.png");
+            //GenderData();
+            string filePath = Path.Combine(_webHostEnvironment.WebRootPath, "LocalData\\VoucherData.json");
+            using StreamReader reader = new(filePath);
+            var json = reader.ReadToEnd();
+            List<Voucher> vouchers = JsonConvert.DeserializeObject<List<Voucher>>(json);
 
-            // Load the small image
-            //Image smallImage = Image.FromFile("wwwroot/images/MOC_004.png");
-
-            // Create graphics from the large image
-            Graphics graphics = Graphics.FromImage(largeImage);
-
-            // Define where you want to place the small image (x, y)
-            int x = 860;
-            int y = 1660;
-
-            for (int i = 1; i <= 10; i++)
-            {
-                string code = "CF_" + i.ToString("D4");
-                string fileName = string.Concat(code, ".png");
-                Image qrCode = Image.FromFile("wwwroot/QRCode/" + fileName);
-                //graphics.DrawImage(qrCode, new Point(0, 0));
-                // Draw the small image on the large image
-                graphics.DrawImage(qrCode, new Point(x, y));
-                largeImage.Save("wwwroot/images/" + code + "1" + ".png");
-            }
-
-            // Save the new image
-
-            // Clean up
-            graphics.Dispose();
-
-
-            return View();
+            //ExportVouchersToExcel(vouchers, Path.Combine(_webHostEnvironment.WebRootPath, "LocalData\\VoucherData.xlsx"));
+            MergeImage();
+            return View(vouchers);
         }
 
         public IActionResult Privacy()
@@ -60,7 +40,7 @@ namespace MergeImage.Controllers
             {
                 string code = "CF_" + i.ToString("D4");
                 string fileName = string.Concat(code, ".png");
-                GeneratedBarcode barcode = QRCodeWriter.CreateQrCode(GenerateRandomString(6), 200);
+                GeneratedBarcode barcode = QRCodeWriter.CreateQrCode(GenerateRandomString(6), 280);
                 barcode.AddAnnotationTextBelowBarcode(code);
                 barcode.SetMargins(10);
                 if (!Directory.Exists(path))
@@ -83,7 +63,81 @@ namespace MergeImage.Controllers
             return new string(Enumerable.Repeat(chars, length)
                 .Select(s => s[random.Next(s.Length)]).ToArray());
         }
+        public void GenderData()
+        {
+            List<Voucher> vouchers = new List<Voucher>();
+            string filePath = Path.Combine(_webHostEnvironment.WebRootPath, "LocalData\\VoucherData.json");
+            //using StreamReader reader = new(filePath);
+            //var json = reader.ReadToEnd();
+            for (int i = 1; i <= 5000; i++)
+            {
+                string key = "CF_" + i.ToString("D4");
+                string code = GenerateRandomString(8);
+                //How to check code is exist in voucher list
+                bool check = vouchers.Any(x => x.Code == code);
+                while (check)
+                {
+                    code = GenerateRandomString(8);
+                    check = vouchers.Any(x => x.Code == code);
+                }
+                vouchers.Add(new Voucher { Key = key, Code = code });
+            }
+            string jsonData = JsonConvert.SerializeObject(vouchers);
+            System.IO.File.WriteAllText(filePath, jsonData);
 
+        }
+        public void MergeImage()
+        {
+            // Load the large image
+            Image largeImage = Image.FromFile("wwwroot/images/c81bb228f0c2519c08d3.png");
+
+            // Load the small image
+            //Image smallImage = Image.FromFile("wwwroot/images/MOC_004.png");
+
+            // Create graphics from the large image
+            Graphics graphics = Graphics.FromImage(largeImage);
+
+            // Define where you want to place the small image (x, y)
+            int x = 780;
+            int y = 1625;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                string code = "CF_" + i.ToString("D4");
+                string fileName = string.Concat(code, ".png");
+                Image qrCode = Image.FromFile("wwwroot/QRCode/" + fileName);
+                //graphics.DrawImage(qrCode, new Point(0, 0));
+                // Draw the small image on the large image
+                graphics.DrawImage(qrCode, new Point(x, y));
+                largeImage.Save("wwwroot/images/" + code + "1" + ".png");
+            }
+
+            // Save the new image
+
+            // Clean up
+            graphics.Dispose();
+        }
+        public void ExportVouchersToExcel(List<Voucher> vouchers, string filePath)
+        {
+            using (ExcelPackage package = new ExcelPackage())
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Vouchers");
+
+                // Write headers
+                worksheet.Cells[1, 1].Value = "Key";
+                worksheet.Cells[1, 2].Value = "Code";
+
+                // Write data
+                for (int i = 0; i < vouchers.Count; i++)
+                {
+                    worksheet.Cells[i + 2, 1].Value = vouchers[i].Key;
+                    worksheet.Cells[i + 2, 2].Value = vouchers[i].Code;
+                }
+
+                // Save to file
+                package.SaveAs(new FileInfo(filePath));
+            }
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ MergeImage/Models/Voucher.cs
@@ -0,0 +1,8 @@
+﻿namespace MergeImage.Models
+{
+    public class Voucher
+    {
+        public string Key { get; set; }
+        public string Code { get; set; }
+    }
+}
